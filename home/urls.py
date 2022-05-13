519ca60beb519e71a9fa3fe67c37e63ea46d3fb7
Skilles <haloeer1@gmail.com>
2022-05-13 15:27:51 -0400

    SEO improvements

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 app_name = 'home'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
