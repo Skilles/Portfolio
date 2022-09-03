0f10ad0da3c21369933636a01368b1a7dae44c9c
Skilles <haloeer1@gmail.com>
2022-09-03 01:59:05 -0400

    Expand char limit for language code

--- langrec/models.py
+++ langrec/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Language(models.Model):
     name = models.CharField(max_length=100)
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=25)
     data = models.JSONField(default=dict)
     major = models.BooleanField(default=False)
     url = models.URLField(default="/")
