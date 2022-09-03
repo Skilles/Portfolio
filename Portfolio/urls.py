1fe4b428d222da2137ec926e500af08854141322
Skilles <haloeer1@gmail.com>
2022-09-03 00:45:37 -0400

    Add health check

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('health/', include('health_check.urls')),
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('langmatch/', include('langrec.urls')),
