b53c8e59dd7274a399ebed76fdf5e82d0e1d6cbc
Skilles <haloeer1@gmail.com>
2022-09-03 03:03:26 -0400

    Change upload_to for project image field

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     languages = models.CharField(max_length=200, default='')
-    image = models.ImageField(default='static/images/default.jpg')
+    image = models.ImageField(default='static/images/default.jpg', upload_to='projects/images/')
     date = models.DateTimeField('date created')
     link = models.URLField(blank=False)
     try_link = models.URLField(blank=True, null=True)
